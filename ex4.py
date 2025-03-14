603ff940da822f4e77f82b35f37e58ec7e241edd
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-14 22:04:15 +0530

    variable addition

--- ex4.py
+++ ex4.py
@@ -2,3 +2,4 @@ cars = 100 # variable with name as cars
 print("cars available",cars,"today")
 truck = 30
 print("trucka available",truck,"this week")
+print("total numbers od trucks and cars ",cars + truck)
