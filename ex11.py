3b4eb8036151f0426f1e4a5e8b1e359f36a59b93
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-14 23:40:01 +0530

    user input for full name

--- ex11.py
+++ ex11.py
@@ -10,7 +10,14 @@
 #pi = 3.14
 #ans = int(radius)*int(radius)*pi
 #print("area of circle is",ans)
-l = input("put the value of length ")
-w = input("put value of width ")
-sol = int(l)*int(w)
-print("the area of rectangle is ",sol)
+#l = input("put the value of length ")
+#w = input("put value of width ")
+#3sol = int(l)*int(w)
+#print("the area of rectangle is ",sol)
+#name = input("what is your name ")
+#age = input("what is your age ")
+#print("your name is",name, "and your age is ",age)
+fist_name = input("what is your firt name ")
+last_name = input("what is your last name ")
+full_name = fist_name + " "+last_name
+print("your full name is",full_name)
