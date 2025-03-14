e347ce0c37e0050653c44b286e7f25dbc8657920
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-14 23:30:50 +0530

    user input for rectangle

--- ex11.py
+++ ex11.py
@@ -6,7 +6,11 @@
 #a = input("enter side of square ")
 #ans = int(a)*int(a)
 #print("area of square is",ans)
-radius = input("put value of radius")
-pi = 3.14
-ans = int(radius)*int(radius)*pi
-print("area of circle is",ans)
+#radius = input("put value of radius")
+#pi = 3.14
+#ans = int(radius)*int(radius)*pi
+#print("area of circle is",ans)
+l = input("put the value of length ")
+w = input("put value of width ")
+sol = int(l)*int(w)
+print("the area of rectangle is ",sol)
