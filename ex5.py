10955f808bc4062c0f5da4e835cbd838a44aa6f0
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-14 23:05:25 +0530

    string variable addition

--- ex5.py
+++ ex5.py
@@ -3,3 +3,7 @@ var2 = "xyz"
 var3 = var1+var2
 print("Addition of string is",var3)
 print("variable 1 is ",var1,"varable 2 is ",var2,"sum of string in var 3 is ",var3)
+a = "nidhi"
+b = "tandan"
+c = a+b
+print("addition of a is",a,"addition of b is",b,"both gives",c)
