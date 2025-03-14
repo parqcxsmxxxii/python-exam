2ee26947edb85da1948fca306c851d97f0af79b5
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-14 23:16:17 +0530

    user input

---
+++ ex11.py
@@ -0,0 +1,8 @@
+#x = input("Put the value of x ")
+#y = input("Put the value of y ")
+#result = int(x)+int(y) #result in interger
+#result = x+y #result =23
+#print("Result of x+y is ",result )
+a = input("enter side of square ")
+ans = int(a)*int(a)
+print("area of square is",ans)
