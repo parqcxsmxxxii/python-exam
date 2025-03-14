0a107b76f38b04f57c2b5fc928c526fd90808a99
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-14 23:25:45 +0530

    user input of circle

--- ex11.py
+++ ex11.py
@@ -3,6 +3,10 @@
 #result = int(x)+int(y) #result in interger
 #result = x+y #result =23
 #print("Result of x+y is ",result )
-a = input("enter side of square ")
-ans = int(a)*int(a)
-print("area of square is",ans)
+#a = input("enter side of square ")
+#ans = int(a)*int(a)
+#print("area of square is",ans)
+radius = input("put value of radius")
+pi = 3.14
+ans = int(radius)*int(radius)*pi
+print("area of circle is",ans)
