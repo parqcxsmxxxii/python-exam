59a25beda057765af38546ee09491698ea859c62
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-14 21:54:02 +0530

    arthmetic operation

--- ex3.py
+++ ex3.py
@@ -1,3 +1,10 @@
 #add =2+3
 
-print(2+3)
+#print(2+3)
+print("addition of two numbers ",5+10)
+print("division of two numbers is",10/2)
+print("multiplication of two numbers is",10*5)
+print("subtraction of two numbers is ",25-15)
+print("addition of three numbers is",2+4+5)
+print("is two number greater than 10",10<15)
+print("is two number greater than 10",10<5)
