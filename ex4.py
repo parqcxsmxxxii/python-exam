2a9f1be263b7626ec433acdeb5c3127ffd425696
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-14 22:09:20 +0530

    variable calculation

--- ex4.py
+++ ex4.py
@@ -3,3 +3,6 @@ print("cars available",cars,"today")
 truck = 30
 print("trucka available",truck,"this week")
 print("total numbers od trucks and cars ",cars + truck)
+space_in_car = 3.0
+carpool_capacity = cars*space_in_car
+print("total carpool capacity",carpool_capacity)
