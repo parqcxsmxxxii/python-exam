c603fe823c67a88585c533615ba3c9debfe891de
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-14 22:51:18 +0530

    arthmetic  variable calculation

---
+++ ex4A.py
@@ -0,0 +1,18 @@
+length=3.5
+breadth = 9
+area_of_rectangle = length*breadth
+print("area of reactale with length",length,"and breath ",breadth,"is equal to",area_of_rectangle)
+radius = 3
+diameter = 6
+pi = 3.14
+area_of_circle = pi*radius*radius
+print("area of circle is ",area_of_circle)
+side=3
+area_of_square = side*side
+print("area of square is",area_of_square,"when its side is 3 ")
+area_of_cube = side*side*side
+print("area of cube is ",area_of_cube)
+base = 10
+height = 5
+area_of_triangle = base*height*0.5
+print("area of triangle is",area_of_triangle,"with its formula")
