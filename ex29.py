ec6a4f96ddcde65765d1af8cf0f3002ba5fe1d57
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-15 20:03:13 +0530

    comparison operator e

---
+++ ex29.py
@@ -0,0 +1,10 @@
+print("Compare two number ")
+input1 = input("enter any number ")
+input2 = input("enter any other number ")
+if int(input1)>int(input2 ):
+    print("input1:",input1,"is grater than input2",input2)
+if int(input1)<int(input2):
+    print("input1:",input1,"is less than input2",input2)
+if int(input1)==int(input2):
+    print("both are equal")
+        
