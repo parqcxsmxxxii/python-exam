8743874f339323498f2134c83eea0faa7ac5fe05
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-15 21:11:52 +0530

    if  else statement

--- ex29.py
+++ ex29.py
@@ -1,10 +1,22 @@
-print("Compare two number ")
-input1 = input("enter any number ")
-input2 = input("enter any other number ")
-if int(input1)>int(input2 ):
-    print("input1:",input1,"is grater than input2",input2)
-if int(input1)<int(input2):
-    print("input1:",input1,"is less than input2",input2)
-if int(input1)==int(input2):
-    print("both are equal")
-        
+#print("Compare two number ")
+#input1 = input("enter any number ")
+#input2 = input("enter any other number ")
+#if int(input1)>int(input2 ):
+    #print("input1:",input1,"is grater than input2",input2)
+#if int(input1)<int(input2):
+    ##print("input1:",input1,"is less than input2",input2)
+#if int(input1)==int(input2):
+   # print("both are equal")
+print("enter a number")
+print("1 is for circle ")
+print("2 for rectangle ")
+print("3 for square ")
+in1 = input("choose circle or rectangle    ")
+if int(in1) == 1:
+    print("output is a circle")
+if int(in1) == 2:
+    print("output is rectangle")
+if int(in1) == 3:
+    print("output is square")
+else:
+    print("Invalid input")        
