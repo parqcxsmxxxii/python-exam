bc558bcc7fe124eef8ea30216ad3737702a512f8
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-15 21:52:08 +0530

    checking string

--- ex29.py
+++ ex29.py
@@ -21,13 +21,21 @@
  #   print("output is square")
 #else:
  #   print("Invalid input") 
-print("check your volume")       
-in1 = input("enter a number between 0 to 100 ")
-if int(in1)>=0 and int(in1)<=30:
-    print("Low volume")
-elif int(in1)>30 and int(in1)<=60:
-    print("medium volume")
-elif int(in1)>60 and int(in1)<=100:
-    print("high volume")    
+#print("check your volume")       
+#in1 = input("enter a number between 0 to 100 ")
+#if int(in1)>=0 and int(in1)<=30:
+ #   print("Low volume")
+#elif int(in1)>30 and int(in1)<=60:
+    #print("medium volume")
+#elif int(in1)>60 and int(in1)<=100:
+   # print("high volume")    
+#else:
+#    print("not valid")
+print("your name  ")
+in1 = input(" your name is ram or sham ")
+if in1 == "ram" or in1 == "Ram" or in1=="RAM":
+    print("hello ",  in1)
+elif in1 == "shyam" or in1 == "Shyam" or in1=="SHYAM":
+    print("hello ",  in1)
 else:
-    print("not valid")
+    print("jai shree ram")      
