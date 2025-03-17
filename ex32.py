e876fb3d4f798cf1c503fea399ebba5822f9b920
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-17 22:59:49 +0530

    printing table using  for loop

--- ex32.py
+++ ex32.py
@@ -1,9 +1,13 @@
-count = [ 5,4,3,2,1]
-fruits = ["apple","banna","guava","kiwi","tomato"]
+# count = [ 5,4,3,2,1]
+# fruits = ["apple","banna","guava","kiwi","tomato"]
 #for i in fruits:
    #print("The fruits are ",i)
 #for c in count:
  #   print("the count is ",c)
-jj = ["a","b","c","d","e"]
-for j in jj:
-    print("the jj is ",j)    
+# jj = ["a","b","c","d","e"]
+# for j in jj:
+#     print("the jj is ",j)    
+number = [ 1,2,3,4,5,6,7,8,9,10]
+for n in number:
+    print("the table for 3 is 3x",n,"=",n*3 )
+    #print("the table of 4 is ",n*4)
