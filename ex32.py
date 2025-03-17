07c654f15679ea933fd5c282dd4eb2cc75c7b512
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-17 23:04:59 +0530

    printing table of 5 using for loop

--- ex32.py
+++ ex32.py
@@ -8,7 +8,9 @@
 # for j in jj:
 #     print("the jj is ",j)    
 number = [ 1,2,3,4,5,6,7,8,9,10]
+# for n in number:
+#     print("the table for 3 is 3x",n,"=",n*3 )
+# for n in number:
+#     print("the table of 4 is 4x",n,"=",n*4)
 for n in number:
-    print("the table for 3 is 3x",n,"=",n*3 )
-for n in number:
-    print("the table of 4 is 4x",n,"=",n*4)
+    print("the table of 5 is 5x ",n,"=",n*5)
