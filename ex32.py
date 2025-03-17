252ee13a18b4a61204f1450ba1e6605db55601ba
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-17 23:14:45 +0530

    matrix using for loop

--- ex32.py
+++ ex32.py
@@ -7,10 +7,17 @@
 # jj = ["a","b","c","d","e"]
 # for j in jj:
 #     print("the jj is ",j)    
-number = [ 1,2,3,4,5,6,7,8,9,10]
+#number = [ 1,2,3,4,5,6,7,8,9,10]
 # for n in number:
 #     print("the table for 3 is 3x",n,"=",n*3 )
 # for n in number:
 #     print("the table of 4 is 4x",n,"=",n*4)
-for n in number:
-    print("the table of 5 is 5x ",n,"=",n*5)
+# for n in number:
+#     print("the table of 5 is 5x ",n,"=",n*5)
+print("learning nested loop")
+matrix = [[3,6,9],
+          [2,4,8],
+          [1,3,6]]
+for m in matrix:
+    for n in m:
+        print("the element of  matrix is ",n)
