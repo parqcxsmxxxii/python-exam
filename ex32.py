1c4db617c1ac47cf665b5de5cc245ac2a35673b3
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-17 21:54:57 +0530

    basics of for loop

---
+++ ex32.py
@@ -0,0 +1,9 @@
+count = [ 5,4,3,2,1]
+fruits = ["apple","banna","guava","kiwi","tomato"]
+#for i in fruits:
+   #print("The fruits are ",i)
+#for c in count:
+ #   print("the count is ",c)
+jj = ["a","b","c","d","e"]
+for j in jj:
+    print("the jj is ",j)    
