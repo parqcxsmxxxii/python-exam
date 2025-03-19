2c581fecc5fec10e9abcdb7a117f2ff8103016f2
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-19 23:49:24 +0530

    function parameters and arguments

--- ex21.py
+++ ex21.py
@@ -1,10 +1,10 @@
 #python functions
-def sum(a,b):
-    addition = a+b
+def sum(a,b,c):# a,b,c are function parameters
+    addition = a+b+c
     return addition
 
-def multiplication(a,b):
-    multiply = a*b
+def multiplication(a,b,c):
+    multiply = a*b*c
     return multiply
 
 # x = 6
@@ -16,5 +16,5 @@ def multiplication(a,b):
 # addnum1 = sum(7,8)
 # print("sum of both number is ",addnum1)
 
-multi = multiplication(1025,1021)
+multi = multiplication(2,4,8) # 2,4,8 are function arguments
 print("multiply of the numbers is ",multi)
