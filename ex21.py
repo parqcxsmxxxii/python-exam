0c580b8cbd75d6255ad9fc95b4e318f1ffd604e3
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-19 23:23:05 +0530

    function with returned value

--- ex21.py
+++ ex21.py
@@ -1,23 +1,12 @@
 #python functions
 def sum(a,b):
-    print("Value of a is ",a)
-    print("Value of b is ",b)
-    print("sum of a+b is ",a+b)
-
-def multiply(a1,b1):
-    print("Value of a1 is ",a1)
-    print("Value of b2 is ",b1)
-    print("sum of a+b is ",a1*b1)
-
-def divide(a2,b2):
-    print("Value of a2 is ",a2)
-    print("Value of b2 is ",b2)
-    print("divide of a2/b2is ",a2/b2)
+    addition = a+b
+    return addition
 
 # x = 6
 # y = 3
-sum(6,3)
-multiply(6,3)
 
-divide(6,3)  # function calling of divide with value a2=6,b2=3
+
+addnum = sum(4,5)  # 
+print("Addition function returns is  ",addnum)
 
