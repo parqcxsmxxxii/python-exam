9946e46c8cd37c03ce2f00e8437ec267710024d5
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-19 23:04:27 +0530

    declaration of function after calling wrong

--- ex21.py
+++ ex21.py
@@ -1,9 +1,10 @@
 #python functions
-def sum(a,b):
-    print("Value of a is ",a)
-    print("Value of b is ",b)
-    print("sum of a+b is ",a+b)
+
 
 x = 12
 y = 21
 sum(x,y)
+def sum(a,b):
+    print("Value of a is ",a)
+    print("Value of b is ",b)
+    print("sum of a+b is ",a+b)
