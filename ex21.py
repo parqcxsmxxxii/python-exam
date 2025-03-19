4cb894d2d45e58425bb96171c6557ee3478eeed3
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-19 23:08:44 +0530

    multiply function

--- ex21.py
+++ ex21.py
@@ -1,10 +1,15 @@
 #python functions
-
-
-x = 12
-y = 21
-sum(x,y)
 def sum(a,b):
     print("Value of a is ",a)
     print("Value of b is ",b)
     print("sum of a+b is ",a+b)
+
+def multiply(a1,b1):
+    print("Value of a1 is ",a1)
+    print("Value of b2 is ",b1)
+    print("sum of a+b is ",a1*b1)
+
+x = 12
+y = 21
+sum(x,y)
+multiply(x,y)
