cbd7b5ca0f254ef211cbffe3db1d7f9c222a078d
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-19 23:31:22 +0530

    multiplication function

--- ex21.py
+++ ex21.py
@@ -3,10 +3,18 @@ def sum(a,b):
     addition = a+b
     return addition
 
+def multiplication(a,b):
+    multiply = a*b
+    return multiply
+
 # x = 6
 # y = 3
 
 
-addnum = sum(4,5)  # 
-print("Addition function returns is  ",addnum)
+# addnum = sum(4,5)  # 
+# print("Addition function returns is  ",addnum)
+# addnum1 = sum(7,8)
+# print("sum of both number is ",addnum1)
 
+multi = multiplication(5,2)
+print("multiply of the numbers is ",multi)
