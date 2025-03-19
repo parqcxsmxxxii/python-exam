debda11c334610a7385e59b23e7d2adefcfe6e12
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-19 23:18:32 +0530

    function and function call

--- ex21.py
+++ ex21.py
@@ -9,7 +9,15 @@ def multiply(a1,b1):
     print("Value of b2 is ",b1)
     print("sum of a+b is ",a1*b1)
 
-x = 12
-y = 21
-sum(x,y)
-multiply(x,y)
+def divide(a2,b2):
+    print("Value of a2 is ",a2)
+    print("Value of b2 is ",b2)
+    print("divide of a2/b2is ",a2/b2)
+
+# x = 6
+# y = 3
+sum(6,3)
+multiply(6,3)
+
+divide(6,3)  # function calling of divide with value a2=6,b2=3
+
