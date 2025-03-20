cfedbfe9552d7da14851f07f7dcc30e174546e03
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-20 20:20:01 +0530

    function import module

---
+++ calculate_function.py
@@ -0,0 +1,22 @@
+def sum(a,b):
+    add = a+b
+    return add
+
+def sub(a,b):
+    minus = a-b
+    return minus
+
+def multiplication(a,b):
+    multiply = a*b
+    return multiply 
+
+def division(a,b):
+    divide = a/b
+    return divide
+
+def compare(a,b):
+    if a>b:
+        return True
+    else:
+        return False
+    
--- ex21A.py
+++ ex21A.py
@@ -1,6 +1,11 @@
-from ex21 import multiplication # modules in python
-from ex21 import sum 
-m = multiplication(4,6)
-print("the value of ",m)
-s = sum(5,2)
-print("the value of sum is ",s)
+# from ex21 import multiplication # modules in python
+# from ex21 import sum 
+# m = multiplication(4,6)
+# print("the value of ",m)
+# s = sum(5,2)
+# print("the value of sum is ",s)
+from calculate_function import sum,sub,multiplication,division,compare
+a = sum(5,7)
+print("addition of both numbers is ",a)
+s = sub(5,7)
+print("subtraction of both numbers is ",s)
