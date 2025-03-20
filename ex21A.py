0634cb3e838271d03b24b4ec5b346da087e186f5
paqcxsmxxxii <nidhishreetandan@Nidhishrees-MacBook-Air.local>
2025-03-20 20:22:15 +0530

    function using if statement

--- ex21A.py
+++ ex21A.py
@@ -5,7 +5,9 @@
 # s = sum(5,2)
 # print("the value of sum is ",s)
 from calculate_function import sum,sub,multiplication,division,compare
-a = sum(5,7)
-print("addition of both numbers is ",a)
-s = sub(5,7)
-print("subtraction of both numbers is ",s)
+# a = sum(5,7)
+# print("addition of both numbers is ",a)
+# s = sub(5,7)
+# print("subtraction of both numbers is ",s)
+c = compare(6,3)
+print("the comparison of both num is ",c)
